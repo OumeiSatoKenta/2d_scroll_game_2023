f0dad8b08ad322e4502492bc1ee183ceed66206b
OumeiSatoKenta <satoken.sk.722@gmail.com>
2023-02-08 04:55:34 +0900

    敵：移動処理実装

---
+++ Assets/Script/EnemyCollisionCheck.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 敵が壁などに当たったときに折り返す処理
+public class EnemyCollisionCheck : MonoBehaviour
+{
+    [HideInInspector] public bool isOn = false;
+
+    private string groundTag = "Ground";
+    private string enemyTag  = "Enemy";
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if (collision.tag == groundTag || collision.tag == enemyTag){
+            isOn = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision){
+        if (collision.tag == groundTag || collision.tag == enemyTag){
+            isOn = false;
+        }
+    }
+}
---
+++ Assets/Script/Enemy_type1.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Enemy Type1の挙動に関するスクリプト
+/// </summary>
+public class Enemy_type1 : MonoBehaviour
+{
+    [Header("画面外でも行動する")] public bool nonVisibleAct;
+    [Header("移動速度")] public float speed;
+    [Header("重力")] public float gravity;
+    [Header("接触判定")] public EnemyCollisionCheck checkCollision;
+
+    private Rigidbody2D rb = null;
+    private SpriteRenderer sr = null;
+    private Animator anim = null;
+    private ObjectCollision oc = null;
+    private BoxCollider2D col = null;
+    private bool rightTleftF = false;
+    private bool isDead = false;
+    void Start()
+    {
+        rb   = GetComponent<Rigidbody2D>();
+        sr   = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        oc   = GetComponent<ObjectCollision>();
+        col  = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        if(!oc.playerStepOn){
+            if (sr.isVisible || nonVisibleAct){
+                // 行動する
+                if (checkCollision.isOn){ // 接触判定スクリプトで判定したフラグ
+                    rightTleftF = !rightTleftF;
+                }
+                int xVector = -1; //デフォルトは左方向
+                if (rightTleftF){
+                    xVector = 1;
+                    transform.localScale = new Vector3(-1, 1, 1);
+                } 
+                else {
+                    transform.localScale = new Vector3(1, 1, 1);
+                }
+                rb.velocity = new Vector2(xVector * speed, -gravity);
+            }
+            else {
+                rb.Sleep();
+            }
+        }
+        else {
+            if(!isDead){
+                anim.Play("enemy_type1_down");
+                rb.velocity = new Vector2(0, -gravity);
+                isDead = true;
+                col.enabled = false;
+                Destroy(gameObject, 3f);
+            }
+            else {
+                transform.Rotate(new Vector3(0,0,2));
+            }
+        }
+        
+    }
+}
