fb35d5b4e6a4b774511ea7c4686c01a8ef2dcfaf
OumeiSatoKenta <satoken.sk.722@gmail.com>
2023-02-11 11:13:30 +0900

    SEを鳴らせるようにした

--- Assets/Script/ScoreItem.cs
+++ Assets/Script/ScoreItem.cs
@@ -7,6 +7,7 @@ public class ScoreItem : MonoBehaviour
 {
     [Header("加算するスコア")] public int myScore;
     [Header("プレイヤーの判定")] public PlayerTriggerCheck playerCheck;
+    [Header("アイテム取得時に鳴らすSE")] public AudioClip itemSE;
 
     void Update()
     {
@@ -14,6 +15,7 @@ public class ScoreItem : MonoBehaviour
         if (playerCheck.isOn){
             if(GManager.instance != null){
                 GManager.instance.score += myScore;
+                GManager.instance.PlaySE(itemSE);
                 Destroy(this.gameObject);
             }
         }        
